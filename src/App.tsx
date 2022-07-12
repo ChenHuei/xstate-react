1c17d623fb082c4c404c566f50973f0189e141b3
LeoChen <leo_chen@mayohr.com>
2022-07-12 18:04:24 +0800

    add xstate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
 
-function App() {
+const toggleMachine = createMachine({
+  id: "toggle",
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      on: { TOGGLE: "inactive" },
+    },
+  },
+});
+
+const App = () => {
+  const [state, send] = useMachine(toggleMachine);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => send("TOGGLE")}>
+      {state.value === "inactive"
+        ? "Click to activate"
+        : "Active! Click to deactivate"}
+    </button>
   );
-}
+};
 
 export default App;
